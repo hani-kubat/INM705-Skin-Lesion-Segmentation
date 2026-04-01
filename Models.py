c1ec4fa657438c8db3e77d9ff55a392d7269045e
hani-kubat <hanifenurk@hotmail.com>
2026-04-01 13:34:16 +0000

    Add Condition D: UNet++ with scSE attention

--- Models.py
+++ Models.py
@@ -1,16 +1,49 @@
 import torch
 import torch.nn as nn
 import segmentation_models_pytorch as smp
+import torch.nn.functional as F
+
+
+class AttentionGate(nn.Module):
+    """
+    Attention Gate module as proposed in Oktay et al. (2018)
+    Attention U-Net: Learning Where to Look for the Pancreas
+    """
+    def __init__(self, F_g, F_l, F_int):
+        super(AttentionGate, self).__init__()
+        self.W_g = nn.Sequential(
+            nn.Conv2d(F_g, F_int, kernel_size=1, stride=1, padding=0, bias=True),
+            nn.BatchNorm2d(F_int)
+        )
+        self.W_x = nn.Sequential(
+            nn.Conv2d(F_l, F_int, kernel_size=1, stride=1, padding=0, bias=True),
+            nn.BatchNorm2d(F_int)
+        )
+        self.psi = nn.Sequential(
+            nn.Conv2d(F_int, 1, kernel_size=1, stride=1, padding=0, bias=True),
+            nn.BatchNorm2d(1),
+            nn.Sigmoid()
+        )
+        self.relu = nn.ReLU(inplace=True)
+
+    def forward(self, g, x):
+        g_resized = F.interpolate(g, size=x.shape[2:], mode='bilinear', align_corners=False)
+        g1 = self.W_g(g_resized)
+        x1 = self.W_x(x)
+        psi = self.relu(g1 + x1)
+        psi = self.psi(psi)
+        return x * psi
 
 
 def build_model(condition='A'):
     """
-    Build U-Net model based on experimental condition.
+    Build model based on experimental condition.
     Condition A: Pretrained ResNet-34 encoder, FROZEN
     Condition B: Pretrained ResNet-34 encoder, FINE-TUNED
     Condition C: Random weights, no pretraining
+    Condition D: UNet++ with scSE attention, pretrained, fine-tuned
     """
-    if condition == 'A' or condition == 'B':
+    if condition == 'A':
         model = smp.Unet(
             encoder_name='resnet34',
             encoder_weights='imagenet',
@@ -18,13 +51,21 @@ def build_model(condition='A'):
             classes=1,
             activation=None
         )
-        if condition == 'A':
-            for param in model.encoder.parameters():
-                param.requires_grad = False
-            print('Condition A: Pretrained encoder FROZEN')
-        else:
-            print('Condition B: Pretrained encoder FINE-TUNED')
-    else:
+        for param in model.encoder.parameters():
+            param.requires_grad = False
+        print('Condition A: Pretrained encoder FROZEN')
+
+    elif condition == 'B':
+        model = smp.Unet(
+            encoder_name='resnet34',
+            encoder_weights='imagenet',
+            in_channels=3,
+            classes=1,
+            activation=None
+        )
+        print('Condition B: Pretrained encoder FINE-TUNED')
+
+    elif condition == 'C':
         model = smp.Unet(
             encoder_name='resnet34',
             encoder_weights=None,
@@ -34,6 +75,20 @@ def build_model(condition='A'):
         )
         print('Condition C: Random weights, no pretraining')
 
+    elif condition == 'D':
+        model = smp.UnetPlusPlus(
+            encoder_name='resnet34',
+            encoder_weights='imagenet',
+            in_channels=3,
+            classes=1,
+            activation=None,
+            decoder_attention_type='scse'
+        )
+        print('Condition D: UNet++ with scSE attention, pretrained fine-tuned')
+
+    trainable = sum(p.numel() for p in model.parameters() if p.requires_grad)
+    total = sum(p.numel() for p in model.parameters())
+    print(f'  Trainable params: {trainable:,} / Total: {total:,}')
     return model
 
 
